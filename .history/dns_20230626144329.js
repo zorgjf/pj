a2b9f6823826e32e04fa7b0484519377435fbf84
zzkjy <mrmrgga@gmail.com>
2023-06-26 14:46:09 +0900

    Revert "asdf"
    This reverts commit 04e1781530cb62ba84ac058816f0acf2e0168138.

--- .history/dns_20230626144329.js
+++ .history/dns_20230626144329.js
@@ -1,6 +1,6 @@
 dnswl = 1231111
 zzz = 11111111
 
-asdsad = 'fassaf11doaldzzziazzzzzzzzzzzzzzzz'
-dk='zxcvagkwlazzzkzzzzzzzzzzzzzzzzz'
+asdsad = '11doaldzzziazzzzzzzzzzzzzzzz'
+dk='gkwlazzzkzzzzzzzzzzzzzzzzz'
 
